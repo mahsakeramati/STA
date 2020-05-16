0e2cae199e406723eaf5f2736aaadf214656cf25
mahsa.keramatee@gmail.com <mahsa.keramatee@gmail.com>
2020-05-16 10:29:46 -0700

    scipy deprecated

--- data.py
+++ data.py
@@ -4,7 +4,9 @@ import tensorpack
 import time
 import random
 import numbers
-from scipy.misc import imread, imresize
+import imageio
+import skimage
+#from scipy.misc import imread, imresize
 import tensorlayer as tl
 from six.moves import cPickle
 from utilities import *
@@ -90,9 +92,9 @@ class BaseImageDataset(BaseDataset):
         super(BaseImageDataset, self).__init__(is_train, skip_pred, transform, sample_weight=sample_weight)
 
     def _get_one_data(self, data, label):
-        im = imread(data, mode='RGB')
+        im = imageio.imread(data,mode="RGB")
         if self.imsize:
-            im = imresize(im, (self.imsize, self.imsize))
+            im = skimage.transform.resize(im,(self.imsize, self.imsize))
         return im, label
 
 
